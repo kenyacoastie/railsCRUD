f5f3e0212f352e927f0e80898d9d485816b195bd
Luke McDonald <t.luke.mcdonald@gmail.com>
2018-01-12 18:34:51 -0500

    commit

--- railsCRUD2/config/initializers/omniauth.rb
+++ railsCRUD2/config/initializers/omniauth.rb
@@ -3,7 +3,7 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
 
-  provider :fitbit,        ENV['fitbit_key'], ENV['fitbit_secret']
+  provider :fitbit,        ENV['fitbit_key'], ENV['fitbit_secret'], scope: "activity profile"
   provider :facebook,      ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 
 end
