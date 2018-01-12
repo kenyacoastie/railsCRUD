39ac693d7c0052a718465b885ff6d3c7e1d9739a
Luke McDonald <t.luke.mcdonald@gmail.com>
2018-01-12 18:12:51 -0500

    added auth and bulma packs"

---
+++ railsCRUD2/config/initializers/omniauth.rb
@@ -0,0 +1,9 @@
+
+
+# config/initializers/omniauth.rb
+Rails.application.config.middleware.use OmniAuth::Builder do
+
+  provider :fitbit,        ENV['fitbit_key'], ENV['fitbit_secret']
+  provider :facebook,      ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+
+end
--- railsCRUD2/config/routes.rb
+++ railsCRUD2/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+
   get 'home/index'
 
+  # config/routes.rb
+  mount_devise_token_auth_for 'User', at: 'auth'
+
   resources :notes
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
 end
