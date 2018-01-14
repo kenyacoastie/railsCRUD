35186eb3c6f86e8cd12735b706847895af1e2763
Luke McDonald <t.luke.mcdonald@gmail.com>
2018-01-14 16:21:41 -0500

    not sure why its not working

--- railsCRUD2/app/models/user.rb
+++ railsCRUD2/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  has_many :notes
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :confirmable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+  has_many :notes
 end
 
